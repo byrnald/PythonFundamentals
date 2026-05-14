063487e82d8215b0b75b8eb2dea5ec42b478a9ff
Isra <byrnald@gmail.com>
2026-05-13 23:48:46 -0400

    Week 2 of the roadmap project, Number Guessing Game

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("testing")
+import random
+
+computer_choice = random.randint(1,100)
+counter = 0
+user_input = 0
+
+while user_input != computer_choice:
+    try:
+        user_input = int(input("Enter your guess (0-100): "))
+        counter+=1
+    except ValueError:
+        print("Only Ints!")
+        continue
+
+    if user_input > computer_choice:
+        print("Too high")
+    elif user_input < computer_choice:
+        print("Too low!")
+    else:
+        print("You chose right!")  
+        if  1 <= counter <= 5:
+            print(f"Guesses: {counter}. Amazing!")
+        elif 6 <=  counter <= 10:
+            print(f"Guesses: {counter}. Good!")
+        else:
+            print(f"Guesses: {counter}. Keep practicing!")
+        
